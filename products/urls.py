b08106ef1afbd500290a0dcfdba488ccf0f63d5f
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-04-28 16:06:31 +0300

    admin

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProductViewSet
+from .views import ProductViewSet, UserAPIView
 
 urlpatterns = [
     path('products', ProductViewSet.as_view({
@@ -11,4 +11,5 @@ urlpatterns = [
         'put': 'update',
         'delete': 'destroy'
     })),
+    path('user', UserAPIView.as_view())
 ]
